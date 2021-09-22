437188cb373070091e630ecb2fe22ca4a9fbfe2e
Jonathan Shapiro <shapiroj18@gmail.com>
2021-09-21 22:30:12 -0400

    added emails

--- chores.go
+++ chores.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/smtp"
 	"os"
@@ -39,13 +40,30 @@ func goDotEnvVariable(key string) string {
 	return val
 }
 
-func Adder(a, b int) int {
-	return a + b
+func mainEmail() {
+	gmail_user := goDotEnvVariable("GMAIL_USER")
+	gmail_pass := goDotEnvVariable("GMAIL_PASS")
+	jonathan_email := goDotEnvVariable("JONATHAN_EMAIL")
+	christiana_email := goDotEnvVariable("CHRISTIAN_EMAIL")
+	sarah_email := goDotEnvVariable("SARAH_EMAIL")
+	paul_email := goDotEnvVariable("PAUL_EMAIL")
+
+	google_doc := goDotEnvVariable("CHORES_CHART")
+
+	subject := "Subject: Chores!\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	body := fmt.Sprintf(
+		`
+			<html>
+				<body>
+					Chores are due today! Please check your name off in the <a href="%s">spreadsheet</a> when you have completed them. Also CC smells like wet feet that have been in a swamp all day.
+				</body>
+			</html>
+		`, google_doc)
+
+	SendEmail(gmail_user, gmail_pass, []string{jonathan_email, christiana_email, sarah_email, paul_email}, []byte(subject+mime+body))
 }
 
 func main() {
-	gmail_user := goDotEnvVariable("GMAIL_USER")
-	gmail_pass := goDotEnvVariable("GMAIL_PASS")
-	jonathan_email, christiana_email := goDotEnvVariable("JONATHAN_EMAIL"), goDotEnvVariable("CHRISTIANA_EMAIL")
-	SendEmail(gmail_user, gmail_pass, []string{jonathan_email, christiana_email}, []byte("test email working"))
+	mainEmail()
 }
